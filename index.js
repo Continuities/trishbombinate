745d36079e8e68886a8796606659cd2881d6c66d
Continuities <michael@doublespeakgames.com>
2018-04-15 16:41:28 -0400

    Composition works, but everything is still super fragile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const formidable = require('formidable');
+const { exec } = require('child_process');
 const argv = require('yargs')
     .option('port', {
         describe: 'Port to run on',
@@ -9,6 +10,10 @@ const argv = require('yargs')
 const app = express();
 
 const UPLOAD_DIR = `${__dirname}/uploads`;
+
+function _toGif(filename) {
+    return `${filename.split('.').slice(0, -1).join('.')}.gif`;
+}
 
 app.use(express.static('www'));
 
@@ -20,10 +25,19 @@ app.post('/upload', (req, res) => {
     });
     form.on('file', (name, file) => {
         console.log(`Received file ${file.name}`);
-        res.sendFile(`${UPLOAD_DIR}/${file.name}`);
+        const outName = _toGif(file.name);
+        exec(`convert ${__dirname}/TrishBomb.gif -coalesce null: \\( ${UPLOAD_DIR}/${file.name} -resize 800x450^ -quality 100 -gravity center -extent 800x450 \\) -compose Dst_Over -layers composite -layers optimize ${__dirname}/www/bombs/${outName}`, (err, stdout, stderr) => {
+            console.log(`${err}, ${stdout}, ${stderr}`);
+            console.log(`Trishbombed ${outName}`);
+            res.sendFile(`${__dirname}/www/bombs/${outName}`);
+        });
     });
 });
 
 app.listen(argv.port, () => {
     console.log(`autotrishbomb running on port ${argv.port}`);
 });
+
+// Resize: convert test.jpg -resize 800x450^ -quality 100 -gravity center -extent 800x450 output.gif
+// Compose: convert TrishBomb.gif -coalesce null: resized.jpg -compose Dst_Over -layers composite -layers optimize composed.gif
+// All in one: convert TrishBomb.gif -coalesce null: \( test.jpg -resize 800x450^ -quality 100 -gravity center -extent 800x450 \) -compose Dst_Over -layers composite -layers optimize composed.gif
